e241969665f7205a8cfd6d0334577230af135834
LisaZP <lis.zecuap@gmail.com>
2025-03-23 17:09:06 -0600

    Este es un crud con Spring, Hibernate y PostgreSQL

---
+++ src/main/java/com/apirestm/apirestm/ApirestmApplication.java
@@ -0,0 +1,13 @@
+package com.apirestm.apirestm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestmApplication.class, args);
+	}
+
+}
